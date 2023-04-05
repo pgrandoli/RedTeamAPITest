b16c6c46ebde16197a6a0284337a7d605eba569b
pgrandoli <pgrandoli@hotmail.com>
2023-04-05 14:41:52 -0300

    Se agregan rutas

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello GRT!!')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
